d0da9a70bd914df88ea1e938d5b969a91b08d0ee
Kara Heeb <karaheeb@gmail.com>
2021-12-14 22:28:45 -0500

    added HTML CSS starter code and began outlining JS questions

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var score = document.querySelector(".score");
+var timer = document.querySelector(".timer-text");
+var startButton = document.querySelector(".start-button");
+
+const questions = [
+    {
+        question: "Who?",
+        answers: {
+            a: "option 1",
+            b: "option 2",
+            c: "option 3"
+        },
+        correctAnswer: "c"
+    },
+    {
+        questions: "What?",
+        answers: {
+            a: "option 1",
+            b: "option 2",
+            c: "option 3"
+        },
+        correctAnswer: "b"
+    }
+]

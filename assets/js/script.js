cf5fb5cbd6b7e7b4da21ecd35ece2ad19a1c67b9
kheeb <93744725+kheeb@users.noreply.github.com>
2022-02-28 22:07:39 -0500

    cleaned up html, updated questions array, built functions to start game, and display choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,54 @@
 var score = document.querySelector(".score");
 var timer = document.querySelector(".timer-text");
-var startButton = document.querySelector(".start-button");
+var startQuiz = document.querySelector("#startQuiz");
+var questionsEl = document.querySelector('#questions');
+var questionsIndex = 0
 
-const questions = [
+var questions = [
     {
-        question: "Who?",
-        answers: {
-            a: "option 1",
-            b: "option 2",
-            c: "option 3"
-        },
-        correctAnswer: "c"
+        question: "What is the capital of Ohio?",
+            choices: ["Columbus", "Springfield", "Los Angeles"],
+            correctAnswer: 'Columbus',
     },
     {
-        questions: "What?",
-        answers: {
-            a: "option 1",
-            b: "option 2",
-            c: "option 3"
-        },
-        correctAnswer: "b"
+        questions: "What is the capital of Virginia?",
+            choices: ["Henrico", "DC", "Richmond"],
+            correctAnswer: "Richmond",
+    },
+    {
+        questions: "What is the capital of California?",
+            choices: ["Los Angeles", "Sacramento", "San Francisco"],
+            correctAnswer: "Sacramento",
+    },
+];
+
+function displayQuestion(){
+    questionsEl.textContent = questions[questionsIndex].question;
+};
+
+function checkAnswer(event){
+    console.log(event.target.textContent)
+}
+
+
+function displayChoices(){
+    for (let i = 0; i < questions[questionsIndex].choices.length; i++) {
+        const answers = questions[questionsIndex].choices[i];
+        const button = document.createElement('button')
+        button.textContent = answers;
+        button.addEventListener('click', checkAnswer)
+        questionsEl.appendChild(button);
     }
-]
+}
+
+
+
+function startGame(){
+    document.querySelector("#startContainer").className = 'hide';
+    displayQuestion();
+    displayChoices();
+};
+
+startQuiz.addEventListener("click", function(){
+    startGame()
+});
